269a27143a607998723f6d57384bd95fd981872c
jimmy-shin <50227042+jimmy-shin@users.noreply.github.com>
2022-01-11 12:51:08 +0900

    Update wsl_static_ip.ps1

--- wsl_static_ip.ps1
+++ wsl_static_ip.ps1
@@ -1,8 +1,9 @@
-echo "Start sleep for 5 seconds..."
-Start-Sleep -Seconds 5
+echo "Start sleep for 10 seconds..."
+Start-Sleep -Seconds 10
 
 bash.exe -c "sudo service ssh start"
 bash.exe -c "sudo service redis-server start"
+bash.exe -c "sudo service xrdp-sesman start"
 bash.exe -c "sudo service xrdp start"
 bash.exe -c "sudo fallocate -l 8G /swapfile"
 bash.exe -c "sudo chmod 600 /swapfile"
@@ -21,24 +22,28 @@ else
     echo "The Script Exited, the ip address of WSL 2 cannot be found";
     exit;
 }
-#[Ports]
-#All the ports you want to forward separated by coma
-$ports=@(50003);
+#[host_ports]
+#All the host_ports you want to forward separated by coma
+$host_ports=@(50003, 50004);
+
+#[wsl_ports]
+$wsl_ports=@(22, 3389);
 
 #[Static ip]
 #You can change the addr to your ip config to listen to a specific address
 $addr='0.0.0.0';
-$ports_a = $ports -join ",";
+$host_ports_a = $host_ports -join ",";
 
 #Remove Firewall Exception Rules
 iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
 
 #adding Exception Rules for inbound and outbound Rules
-iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
-iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
-for( $i = 0; $i -lt $ports.length; $i++ )
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $host_ports_a -Action Allow -Protocol TCP";
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $host_ports_a -Action Allow -Protocol TCP";
+for( $i = 0; $i -lt $host_ports.length; $i++ )
 {
-    $port = $ports[$i];
-    iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
-    iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=22 connectaddress=$remoteport";
+    $host_port = $host_ports[$i];
+    $wsl_port = $wsl_ports[$i];
+    iex "netsh interface portproxy delete v4tov4 listenport=$host_port listenaddress=$addr";
+    iex "netsh interface portproxy add v4tov4 listenport=$host_port listenaddress=$addr connectport=$wsl_port connectaddress=$remoteport";
 }
